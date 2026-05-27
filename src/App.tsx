7241c43df557f5c590a777bdb720b8f82353073c
Theo Rondon <theoprondon@hotmail.com>
2026-05-26 22:42:51 -0300

    feat: Criando o componente Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import InputText from './components/input-text';
 import InputCheckbox from './components/input-checkbox';
 import Card from './components/card';
 import Container from './components/container';
+import Skeleton from './components/skeleton';
 import TrashIcon from './assets/icons/trash.svg?react';
 import CheckIcon from './assets/icons/check.svg?react';
 import PencilIcon from './assets/icons/pencil.svg?react';
@@ -61,6 +62,12 @@ export default function App() {
 
         <div>
           <Card size="md">Olá mundo</Card>
+        </div>
+
+        <div className="space-y-2">
+          <Skeleton className="h-6" />
+          <Skeleton className="h-6" />
+          <Skeleton className="w-96 h-6" />
         </div>
         
       </div>
---
+++ src/components/skeleton.tsx
@@ -0,0 +1,29 @@
+import { type VariantProps, cva } from "class-variance-authority";
+
+export const skeletonVariants = cva(`
+  animate-pulse bg-gray-200
+`, {
+  variants: {
+    rounded: {
+      sm: "rounded-sm",
+      lg: "rounded-lg",
+      full: "rounded-full"
+    }
+  },
+  defaultVariants: {
+    rounded: "lg"
+  }
+});
+
+interface SkeletonProps extends VariantProps<typeof skeletonVariants>,
+  React.ComponentProps<"div"> {}
+
+export default function Skeleton({
+  rounded,
+  className,
+  ...props
+}: SkeletonProps) {
+  return <div className={skeletonVariants({rounded, className})}
+  
+  />
+}

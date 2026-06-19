196087be9b666dccac230d8b70e0584590d7a15a
Theo Rondon <theoprondon@hotmail.com>
2026-06-18 23:02:06 -0300

    feat: Preparando a tarefa na lista

---
+++ src/hooks/use-task.ts
@@ -0,0 +1,19 @@
+import useLocalStorage from "use-local-storage-state";
+import { TASKS_KEY, type Task, TaskState } from "../models/task";
+
+
+export default function useTask() {
+    const [tasks, setTasks] = useLocalStorage<Task[]>(TASKS_KEY, { defaultValue: [] });
+
+    function prepareTask() {
+        setTasks([...tasks, {
+            id: Math.random().toString(26).substring(2, 9), 
+            title: "", 
+            state: TaskState.Creating
+        }]);
+    }
+
+    return {
+        prepareTask,
+    }
+}
